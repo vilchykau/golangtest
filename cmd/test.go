9229a2f5cc520a07181e76d4cce7ad88e001fd38
vilchykau <donsgenii@gmail.com>
2022-11-17 17:05:40 +0300

    feat: rewrite to use app.go

--- cmd/test.go
+++ cmd/test.go
@@ -1,11 +1,26 @@
-package cmd_test
+package main
 
-import (
-	"fmt"
+// "context"
 
-	"../pkg/htmlparser"
-)
+// "github.com/vilchykau/golangtest/internal/db/model"
+// "github.com/vilchykau/golangtest/internal/db/price"
+// "github.com/vilchykau/golangtest/internal/drivers"
+
+import "github.com/vilchykau/golangtest/internal/app"
 
 func main() {
-	fmt.Println(htmlparser.ParserPrice("asdf"))
+	// ctx := context.Background()
+
+	// driver :=
+	// 	drivers.NewPgDriver(drivers.PgConfig{DnsString: "User ID=habrpguser;Password=pgpwd4habr;Host=127.0.0.1;Port=5432;Database=habrdb;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;"})
+	// driver.Connect(ctx)
+
+	// url := "SomeUrl"
+	// var price1 float64 = 1233.4
+
+	// priceMan := price.NewPrice(driver.GetDb())
+	// priceMan.InsertPrice(ctx, &model.Price{Url: &url, Price: &price1})
+
+	app := &app.App{}
+	app.Start()
 }
