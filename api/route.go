7f94fe41b67d61f59932d294961963022c0b0d50
vilchykau <donsgenii@gmail.com>
2022-11-17 17:02:31 +0300

    init: create api init file

---
+++ api/route.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vilchykau/golangtest/api/subs"
+)
+
+func initSubs(engine *gin.Engine) {
+	g := engine.Group("/api/v1/subs/")
+	subs.InitGroup(g)
+}
+
+func InitGroups(engine *gin.Engine) {
+	initSubs(engine)
+}
